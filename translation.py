2ee51594eea622496f70bf0079f13bdd540be566
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-15 21:14:37 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,9 +3,9 @@ from config import Config
 
 class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot V2
-This Bot forward all files to One Public channel to Your Personal channel
-More details /help</i>"""
+<i>I'm Simple Auto file Forward Bot V3.9
+This Bot forward all files to One Public or Privet channel to Your Personal channel
+This Bot Is Privet</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!
 • User Join From channel Must(No need Admin permission)
